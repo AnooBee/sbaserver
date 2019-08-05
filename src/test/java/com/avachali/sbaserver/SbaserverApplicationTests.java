92e551e1e9e9c574e8f810c160b27bad86ae6f8c
avachali <avachali@gmail.com>
2019-08-04 20:32:36 -0400

    Initial commit

---
+++ src/test/java/com/avachali/sbaserver/SbaserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.avachali.sbaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbaserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
